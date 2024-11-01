8e448f78f5f79b35d44293b833834888074f76ef
deathesther <122229257+Esther7171@users.noreply.github.com>
2024-11-01 22:26:59 +0530

    Update secret.py

--- Madness/secret.py
+++ Madness/secret.py
@@ -2,11 +2,11 @@
 
 import requests
 
-host = 'IP'
+host = input("Enter IP:")
 url = 'http://{}/th1s_1s_h1dd3n/?secret={}'
 
 for i in range(100):
     r = requests.get(url.format(host, i))
-    if not 'That is wrong!' in r.text:
+    if not 'wrong' in r.text:
         print("Found secret: {}".format(i))
         print(r.text)
